3a4525fd36f50798d2b0df68d5f831b52f2ff7d8
DESKTOP-EIVOOOA\ASUS <qkrwnsdh00@gmail.com>
2023-08-29 10:52:04 +0900

    Feat: LobbyManager Script Start(), OnConnectedToMaster() event

--- Zombie Multiplayer/Assets/Scripts/LobbyManager.cs
+++ Zombie Multiplayer/Assets/Scripts/LobbyManager.cs
@@ -5,23 +5,40 @@ using UnityEngine.UI;
 
 // 마스터(매치 메이킹) 서버와 룸 접속을 담당
 public class LobbyManager : MonoBehaviourPunCallbacks {
-    private string gameVersion = "1"; // 게임 버전
+    private string gameVersion = "1.0.0"; // 게임 버전
 
     public Text connectionInfoText; // 네트워크 정보를 표시할 텍스트
     public Button joinButton; // 룸 접속 버튼
 
     // 게임 실행과 동시에 마스터 서버 접속 시도
-    private void Start() {
-        
+    private void Start()
+    {
+        //접속에 필요한 정보 설정
+        PhotonNetwork.GameVersion = gameVersion;
+
+        // 설정한 정보로 마스터 서버 접속 시도
+        PhotonNetwork.ConnectUsingSettings();
+
+        // 룸 접속 버튼 잠시 비활성화
+        joinButton.interactable = false;
+
+        // 접속 시도 중 임을 텍스트로 표시
+        connectionInfoText.text = "Connect to master server ...";
     }
 
     // 마스터 서버 접속 성공시 자동 실행
-    public override void OnConnectedToMaster() {
-        
+    public override void OnConnectedToMaster()
+    {
+        // 룸 접속 버튼 활성화
+        joinButton.interactable = true;
+
+        // 접속 정보 표시하기
+        connectionInfoText.text = "Online: Connected to mater server succeed";
     }
 
     // 마스터 서버 접속 실패시 자동 실행
-    public override void OnDisconnected(DisconnectCause cause) {
+    public override void OnDisconnected(DisconnectCause cause)
+    {
         
     }
 
@@ -31,12 +48,14 @@ public class LobbyManager : MonoBehaviourPunCallbacks {
     }
 
     // (빈 방이 없어)랜덤 룸 참가에 실패한 경우 자동 실행
-    public override void OnJoinRandomFailed(short returnCode, string message) {
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
         
     }
 
     // 룸에 참가 완료된 경우 자동 실행
-    public override void OnJoinedRoom() {
+    public override void OnJoinedRoom()
+    {
         
     }
 }
